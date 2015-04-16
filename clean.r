da7a1f281707702b3d4d1a1d1d24504a531da9fd
ixaxaar <root@ixaxaar.in>
2015-04-17 03:16:04 +0530

    Add more cleaning techniques

--- clean.r
+++ clean.r
@@ -1,47 +1,44 @@
 
 # Identify the dataset
-names(dat) <- normVarNames(names(dat))
+names(dat) = normVarNames(names(dat))
 # Lower case variable names.
-vars <- names(dat)
-target <- "rain_tomorrow"
-risk <- "risk_mm"
-id <- c("date", "location")
+vars = names(dat)
+target = "rain_tomorrow"
+risk = "risk_mm"
+id = c("date", "location")
 
 # Ignore the IDs and the risk variable.
-ignore <- c(id, if (exists("risk")) risk)
+ignore = c(id, if (exists("risk")) risk)
 
 # Ignore variables which are completely missing.
-mvc <- sapply(dat[vars], function(x) sum(is.na(x)))
-mvn <- names(which(mvc == nrow(dat)))
-ignore <- union(ignore, mvn)
+mvc = sapply(dat[vars], function(x) sum(is.na(x)))
+mvn = names(which(mvc == nrow(dat)))
+ignore = union(ignore, mvn)
 
 # Initialise the variables
-vars <- setdiff(vars, ignore)
+vars = setdiff(vars, ignore)
 
 # Variable roles.
-inputs <- setdiff(vars, target)
-numi <- which(sapply(dat[inputs], is.numeric))
-numc <- names(numi)
-cati <- which(sapply(dat[inputs], is.factor))
-catc <- names(cati)
+inputs = setdiff(vars, target)
+numi = which(sapply(dat[inputs], is.numeric))
+numc = names(numi)
+cati = which(sapply(dat[inputs], is.factor))
+catc = names(cati)
 
 # Remove all observations with a missing target.
-dat <- dat[!is.na(dat[target]),]
+dat = dat[!is.na(dat[target]),]
 
 # Impute missing values needed for randomForest().
-if (sum(is.na(dat[vars]))) dat[vars] <- na.roughfix(dat[vars])
+if (sum(is.na(dat[vars]))) dat[vars] = na.roughfix(dat[vars])
 
 # Ensure the target is categoric.
-dat[target] <- as.factor(dat[[target]])
+dat[target] = as.factor(dat[[target]])
 
 # Number of observations.
-nobs <- nrow(dat)
+nobs = nrow(dat)
 
-# Prepare for model building.
-form <- formula(paste(target, "~ ."))
-seed <- 328058
-train <- sample(nobs, 0.7*nobs)
-test <- setdiff(seq_len(nobs), train)
-actual <- dat[test, target]
-risks <- dat[test, risk]
+# Detect skews in the data
+require(e1071)
+apply(dat, 2, skewness)
+
 
