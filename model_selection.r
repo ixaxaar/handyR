f52dd509802a822a00c0903c9d65c6547cb77f72
ixaxaar <root@ixaxaar.in>
2015-04-20 04:07:42 +0530

    Move ROC curves to model_seletction

--- model_selection.r
+++ model_selection.r
@@ -26,3 +26,20 @@ RMSE(predicted, observed)
 cor(predicted, observed)
 cor(predicted, observed, method = "spearman")
 
+roc_curve <- function(pr.m)
+{
+  pr <- prediction(pr.m, actual)
+  pe <- performance(pr, "tpr", "fpr")
+  au <- performance(pr, "auc")@y.values[[1]]
+  pd <- data.frame(fpr=unlist(pe@x.values), tpr=unlist(pe@y.values))
+
+  p <- ggplot(pd, aes(x=fpr, y=tpr))
+  p <- p + geom_line(colour="red")
+  p <- p + xlab("False Positive Rate") + ylab("True Positive Rate")
+  p <- p + ggtitle(deparse(substitute(pr.m)))
+  p <- p + annotate("text", x=0.50, y=0.00, hjust=0, vjust=0, size=5,
+  label=paste("Area under curve =", round(au, 2)))
+
+  return(p)
+}
+
